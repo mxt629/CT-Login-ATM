06feb5a4004bbb7011cf59f803c17df2feb3bf4e
mxt629 <xuantien.06021990@gmail.com>
2022-03-23 08:07:57 +0700

    Initial Commit

---
+++ CT Login ATM/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CT Login ATM
+//
+//  Created by Little Chick on 23/03/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
